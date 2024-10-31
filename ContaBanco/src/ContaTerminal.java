186ce4543d7919bf17d30902beb2effcef7cfb4a
geissa carvalho <geissaocarvalho@gmail.com>
2024-10-31 11:17:57 -0300

    Desafio concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //TODO:Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuario
+
+        //Obter pela scanner os valores digitados pelo terminal
+
+        //Exibir a mensagem conta criada
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        string agencia;
+        string nomeCliente;
+        double saldo;
+
+
+
+
     }
 }
