45f0aa2b9bc881f4ff9264930308e358e2a1f6bf
geissa carvalho <geissaocarvalho@gmail.com>
2024-10-31 11:22:13 -0300

    Desafio Concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,7 +4,8 @@ public class ContaTerminal {
     public static void main(String[] args) {
         //TODO:Conhecer e importar a classe Scanner
 
-        //Exibir as mensagens para o nosso usuario
+        //Exibir as mensagens para o nosso usuario123
+        
 
         //Obter pela scanner os valores digitados pelo terminal
 
@@ -13,12 +14,40 @@ public class ContaTerminal {
         Scanner sc = new Scanner(System.in);
 
         int numero;
-        string agencia;
-        string nomeCliente;
+        String agencia;
+        String nomeCliente;
         double saldo;
 
+        System.out.println("---------------CONTA BANCÁRIA---------------");
+		System.out.println();
+		System.out.println("Por favor, digite o número da Conta: (1234)");
+		numero = sc.nextInt();
+
+		System.out.println("Agora digite o número da Agência: (123-8)");
+		agencia = sc.next();
+
+		System.out.println("Me informe seu nome: ");
+		nomeCliente = sc.next();
+		sc.nextLine();
+
+		System.out.println("Agora me informa seu saldo atual: ");
+		saldo = sc.nextDouble();
+
+		System.out.println();
+		System.out.printf("Olá %s, obrigado por criar uma conta em "
+				+ "nosso banco, sua agência é %s, "
+				+ "conta %d e seu saldo %.2f já está disponível"
+				+ " para saque.", nomeCliente, agencia, numero, saldo);
+
+		sc.close();
 
 
 
+    }
+
+    private static class string {
+
+        public string() {
+        }
     }
 }
